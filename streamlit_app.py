a4c65ae0f5b7a575c51bca39cacad09b565fb781
Yamuna-Saravanan <130555375+Yamuna-Saravanan@users.noreply.github.com>
2023-04-12 16:32:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breafast Menu')
+streamlit.text('Omega 3 & Bluberry oat meal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-boiled Free-Range egg')
 
